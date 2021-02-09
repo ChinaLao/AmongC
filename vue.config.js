84f560228945ef30c578854587ead895171873ff
China Lao <china.appsell@gmail.com>
2021-02-09 08:28:16 +0800

    updated colors, name and icons

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,15 @@
 module.exports = {
-  transpileDependencies: [
-    'vuetify'
-  ]
-}
+	transpileDependencies: ["vuetify"],
+	pwa: {
+		name: "Among C",
+		themeColor: "#0E0D47",
+		msTileColor: "#0E0D47",
+		iconPath: {
+			favicon32: "img/icons/favicon-32x32.png",
+			favicon16: "img/icons/favicon-16x16.png",
+			appleTouchIcon: "img/icons/apple-touch-icon-152x152.png",
+			maskIcon: "img/icons/safari-pinned-tab.svg",
+			msTileImage: "img/icons/msapplication-icon-144x144.png",
+		},
+	},
+};
