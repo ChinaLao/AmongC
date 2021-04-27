f504e9b3fe3010f5d01c0407ad2c3357f5b89792
China <chinamarielao09@gmail.com>
2021-04-27 16:20:50 +0800

    allowed empty str

--- src/store/modules/lexicalAnalyzer.js
+++ src/store/modules/lexicalAnalyzer.js
@@ -47,7 +47,7 @@ export default {
       newline: {match: /\n|\r\n|\r/, lineBreaks: true},
       whitespace: /[ \t]+/,
 
-      litStr: /[\\"].+[\\"]/,
+      litStr: /[\\"].*[\\"]/,
       singleComment: /#.*/,
       litDec: /[~]?[0-9]{1,9}[.][0-9]{1,5}/,
       negaLitInt: /[~][0-9]{1,9}/,
