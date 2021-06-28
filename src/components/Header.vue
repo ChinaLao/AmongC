00922f3b70b14eea8e6a3d6cdb506c49af196836
China <chinamarielao09@gmail.com>
2021-06-28 09:15:20 +0800

    added documentation

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,15 +4,26 @@
 		absolute
 		color="primary"
 		dark
-		class="text-h4 font-weight-bold"
 	>
 		<v-row>
-			<v-avatar class="mx-5" tile size="50">
-				<v-img
-					:src="require('@/assets/logo.png')"
-				></v-img>
-			</v-avatar>
-			<span class="mt-1">Among C</span>
+			<v-col class="mt-1" cols="10">
+				<v-avatar class="mx-5 mt-n3" tile size="50">
+					<v-img
+						@click="goto('Home')"
+						:src="require('@/assets/logo.png')"
+					></v-img>
+				</v-avatar>
+				<span 
+					class="mt-1 text-h4 font-weight-bold" 
+					@click="goto('Home')"
+				>Among C</span>
+			</v-col>
+			<v-col class="ml-15 mt-3" cols="1">
+				<span 
+					class="mt-3" 
+					@click="goto('Documentation')"
+				>Documentation</span>
+			</v-col>
 		</v-row>
 	</v-app-bar>
 </template>
@@ -20,6 +31,11 @@
 <script>
 	export default {
 		name: "Header",
+		methods: {
+			goto(val){
+				this.$router.push({ name: val});
+			}
+		}
 	};
 </script>
 
--- src/views/Documentation.vue
+++ src/views/Documentation.vue
@@ -2,7 +2,7 @@
 	<v-app>
 		<Header />
 		<v-main>
-			This is about page and this is very waw
+			<iframe src='https://docs.google.com/document/d/1Y96vCP5-77jvFhs2KkKJKy8S77b1wkaraE7Ckium6i0/edit?usp=sharing' width='1366px' height='623px' frameborder='0'></iframe>
 		</v-main>
 		<v-footer></v-footer>
 	</v-app>
@@ -11,7 +11,7 @@
 <script>
 	import Header from "@/components/Header.vue";
 	export default {
-		name: "About",
+		name: "Documentation",
 		components: {
 			Header,
 		},
