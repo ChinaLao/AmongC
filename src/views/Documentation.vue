6dd88197fcc46b2df239049fde1c2d23e926d3b9
China <chinamarielao09@gmail.com>
2021-06-28 10:16:49 +0800

    fixed size of iframe

--- src/views/Documentation.vue
+++ src/views/Documentation.vue
@@ -2,7 +2,7 @@
 	<v-app>
 		<Header />
 		<v-main>
-			<iframe src='https://docs.google.com/document/d/1Y96vCP5-77jvFhs2KkKJKy8S77b1wkaraE7Ckium6i0/edit?usp=sharing' width='1366px' height='623px' frameborder='0'></iframe>
+			<iframe src='https://docs.google.com/document/d/1Y96vCP5-77jvFhs2KkKJKy8S77b1wkaraE7Ckium6i0/edit?usp=sharing' width='1349px' height='580px' frameborder='0'></iframe>
 		</v-main>
 		<v-footer></v-footer>
 	</v-app>
