e8c77dd1fac3f81e51c687d55b51589bffb12413
China <chinamarielao09@gmail.com>
2021-06-28 09:25:38 +0800

    added group name

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,23 +4,25 @@
 		absolute
 		color="primary"
 		dark
+		class="text-h4 font-weight-bold"
 	>
-		<v-row>
-			<v-col class="mt-1" cols="10">
-				<v-avatar class="mx-5 mt-n3" tile size="50">
+		<v-row class="mt-n1">
+			<v-col cols="5">
+				<v-avatar class="mx-5 mt-n1" tile size="50">
 					<v-img
 						@click="goto('Home')"
 						:src="require('@/assets/logo.png')"
 					></v-img>
 				</v-avatar>
-				<span 
-					class="mt-1 text-h4 font-weight-bold" 
+				<span
 					@click="goto('Home')"
 				>Among C</span>
 			</v-col>
-			<v-col class="ml-15 mt-3" cols="1">
-				<span 
-					class="mt-3" 
+			<v-col cols="4">
+				<span class="font-italic">dankpink.exe</span>
+			</v-col>
+			<v-col class="ml-12" cols="1">
+				<span
 					@click="goto('Documentation')"
 				>Documentation</span>
 			</v-col>
