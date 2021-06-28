18b545115d2b71dc61c4fdfeeb700dfbee213e4b
China <chinamarielao09@gmail.com>
2021-06-28 09:44:25 +0800

    fixed design

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,27 +4,23 @@
 		absolute
 		color="primary"
 		dark
-		class="text-h4 font-weight-bold"
 	>
-		<v-row class="mt-n1">
-			<v-col cols="5">
-				<v-avatar class="mx-5 mt-n1" tile size="50">
-					<v-img
-						@click="goto('Home')"
-						:src="require('@/assets/logo.png')"
-					></v-img>
-				</v-avatar>
-				<span
-					@click="goto('Home')"
-				>Among C</span>
+		<v-row>
+			<v-col cols="4" class="mr-13 mt-1">
+				<v-btn outlined color="primary" @click="goto('Home')">
+					<v-avatar class="mx-5" tile size="50">
+						<v-img :src="require('@/assets/logo.png')"></v-img>
+					</v-avatar>
+					<span class="text-h4 font-weight-bold white--text">Among C</span>
+				</v-btn>
 			</v-col>
-			<v-col cols="4">
-				<span class="font-italic">dankpink.exe</span>
+			<v-col cols="4" class="ml-16 mt-1">
+				<span class="font-italic text-h4">dankpink.exe</span>
 			</v-col>
-			<v-col class="ml-12" cols="1">
-				<span
-					@click="goto('Documentation')"
-				>Documentation</span>
+			<v-col cols="1" class="ml-16 mt-1">
+				<v-btn outlined color="primary" @click="goto('Documentation')">
+					<span class="text-h5 text-lowercase white--text">Documentation ></span>
+				</v-btn>
 			</v-col>
 		</v-row>
 	</v-app-bar>
