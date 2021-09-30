9a05224bbc2c514f3053cc6a01bcbf1c4f32ade1
China Marie Lao <china.appsell@gmail.com>
2021-09-30 12:45:21 +0800

    updated documentation

--- src/views/Documentation.vue
+++ src/views/Documentation.vue
@@ -2,7 +2,7 @@
 	<v-app>
 		<Header />
 		<v-main>
-			<iframe src='https://docs.google.com/document/d/1a9Gp33z9k2D8gbXX-Vde3UBebsyGtkIs0beKN4bckwo/edit?usp=sharing' width='1349px' height='580px' frameborder='0'></iframe>
+			<iframe src='https://docs.google.com/document/d/1yGf8gRsej8gGvx-Wn3PGVNhSO7nIs5EyqvWulnt2Bck/edit?usp=sharing' width='1349px' height='580px' frameborder='0'></iframe>
 		</v-main>
 		<v-footer></v-footer>
 	</v-app>
